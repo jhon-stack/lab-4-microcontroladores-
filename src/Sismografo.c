41900d873ff700494235b3ae91594a841ce12306
Alex Varela Quirós <98862101+ingalexvarela@users.noreply.github.com>
2023-02-17 23:25:59 -0600

    Listo el entorno grafico

--- src/Sismografo.c
+++ src/Sismografo.c
@@ -1,92 +1,110 @@
-/*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2014 Chuck McManis <cmcmanis@mcmanis.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+// Agregamos Bibliotecas
+#include <stdio.h>
+#include <string.h> 
 #include <stdint.h>
 #include <math.h>
+// Agregamos para el uso de bibliotecas de proyecto libopencm3
 #include "clock.h"
 #include "console.h"
 #include "sdram.h"
 #include "lcd-spi.h"
 #include "gfx.h"
 
-/* Convert degrees to radians */
-#define d2r(d) ((d) * 6.2831853 / 360.0)
 
-/*
- * This is our example, the heavy lifing is actually in lcd-spi.c but
- * this drives that code.
- */
 int main(void)
 {
-	int p1, p2, p3;
-
 	clock_setup();
 	console_setup(115200);
 	sdram_init();
 	lcd_spi_init();
-	console_puts("LCD Initialized\n");
-	console_puts("Should have a checker pattern, press any key to proceed\n");
-	msleep(2000);
-/*	(void) console_getc(1); */
 	gfx_init(lcd_draw_pixel, 240, 320);
-	gfx_fillScreen(LCD_GREY);
-	gfx_fillRoundRect(10, 10, 220, 220, 5, LCD_WHITE);
-	gfx_drawRoundRect(10, 10, 220, 220, 5, LCD_RED);
-	gfx_fillCircle(20, 250, 10, LCD_RED);
-	gfx_fillCircle(120, 250, 10, LCD_GREEN);
-	gfx_fillCircle(220, 250, 10, LCD_BLUE);
-	gfx_setTextSize(2);
-	gfx_setCursor(15, 25);
-	gfx_puts("STM32F4-DISCO");
-	gfx_setTextSize(1);
-	gfx_setCursor(15, 49);
-	gfx_puts("Simple example to put some");
-	gfx_setCursor(15, 60);
-	gfx_puts("stuff on the LCD screen.");
-	lcd_show_frame();
-	console_puts("Now it has a bit of structured graphics.\n");
-	console_puts("Press a key for some simple animation.\n");
-	msleep(2000);
-/*	(void) console_getc(1); */
-	gfx_setTextColor(LCD_YELLOW, LCD_BLACK);
-	gfx_setTextSize(3);
-	p1 = 0;
-	p2 = 45;
-	p3 = 90;
+	
+	// arreglos de caracteres que almacena la lectura del eje X, Y, Z y nivel de bateria
+	char print_x[5];       
+	char print_y[5];        
+	char print_z[5];
+	char print_PILA[5];
+	char print_ONOFF[3];
+	
+	// Valores iniciales para las variables leidas, se declaran para no contener datos basura
+	int x = 110;             // variable inicial para imprimir lectura x
+    	int y = 120;             // variable inicial para imprimir lectura y
+    	int z = 130;	       // variable inicial para imprimir lectura z
+	float nivel_bateria = 7;       // variable inicial para imprimir lectura z
+	char * SerialUSB = "OFF";      // variable inicial para imprimir estado de serial/USB
+	char data[10] = ""; 	       // Inicializa el arreglo "data" con una cadena vacia
+	
 	while (1) {
-		gfx_fillScreen(LCD_BLACK);
-		gfx_setCursor(15, 36);
-		gfx_puts("PLANETS!");
-		gfx_fillCircle(120, 160, 40, LCD_YELLOW);
-		gfx_drawCircle(120, 160, 55, LCD_GREY);
-		gfx_drawCircle(120, 160, 75, LCD_GREY);
-		gfx_drawCircle(120, 160, 100, LCD_GREY);
+		/* formateando variables en cadenas de caracteres
+		para  imprimirlas en LCD */
 
-		gfx_fillCircle(120 + (sin(d2r(p1)) * 55),
-			       160 + (cos(d2r(p1)) * 55), 5, LCD_RED);
-		gfx_fillCircle(120 + (sin(d2r(p2)) * 75),
-			       160 + (cos(d2r(p2)) * 75), 10, LCD_WHITE);
-		gfx_fillCircle(120 + (sin(d2r(p3)) * 100),
-			       160 + (cos(d2r(p3)) * 100), 8, LCD_BLUE);
-		p1 = (p1 + 3) % 360;
-		p2 = (p2 + 2) % 360;
-		p3 = (p3 + 1) % 360;
+		// formateo para "x"
+		sprintf(print_x, "%s", "X:");       // Escribir el string "X:" en la cadena print_x
+		sprintf(data, "%d",  x);    // Convertir lectura.x a STR y almacenarlo en cadena data
+		strcat(print_x, data);              // Agrega el contenido de la cadena data al final de la cadena print_x
+
+		// formateo para "y"
+		sprintf(print_y, "%s", "Y:");
+		sprintf(data, "%d",  y);
+		strcat(print_y, data);
+		
+		// formateo para "z"
+		sprintf(print_z, "%s", "Z:");
+		sprintf(data, "%d",  z);
+		strcat(print_z, data);
+		
+		// formateo para "nivel_bateria"
+		sprintf(print_PILA, "%s", "");
+		sprintf(data, "%f",  nivel_bateria);
+		strcat(print_PILA, data);
+
+		// formateo para "Serial/USB"
+		sprintf(print_ONOFF, "%s", "");
+		sprintf(data, "%s",  SerialUSB);
+		strcat(print_ONOFF, data);
+
+		/* Bloque para visualizar informacion en la pantalla LCD */
+		gfx_fillScreen(LCD_BLACK);     		  // llenar la pantalla con color negro.
+		gfx_setTextColor(LCD_WHITE, LCD_BLACK);   // Establece el color de fondo a negro y el color del texto en verde.
+		//Desplagar en pantalla
+		gfx_setTextSize(2);                       // Size del texto
+		gfx_setCursor(5, 15);                     // posiciona el cursor en la pantalla
+		gfx_puts("SISMOGRAFO");                   // Imprime el texto
+		gfx_setTextColor(LCD_BLUE, LCD_BLACK);
+		gfx_setCursor(180, 15);
+		gfx_puts("UCR");
+		gfx_setTextColor(LCD_YELLOW, LCD_BLACK);
+		gfx_setCursor(80, 50);
+		gfx_puts("Ejes");
+		gfx_setTextSize(1.8);
+		gfx_setCursor(70, 75);
+		gfx_puts("Giroscopio");
+
+		gfx_setTextColor(LCD_GREEN, LCD_BLACK);
+		gfx_setTextSize(2);
+		gfx_setCursor(80, 100);
+		gfx_puts(print_x);                        // Imprime contenido de variable
+		gfx_setCursor(80, 130);
+		gfx_puts(print_y);
+		gfx_setCursor(80, 160);
+		gfx_puts(print_z);
+
+		gfx_setTextColor(LCD_YELLOW, LCD_BLACK);
+		gfx_setCursor(55, 185);
+		gfx_puts("Bateria:");
+		gfx_setTextColor(LCD_GREEN, LCD_BLACK);
+		gfx_setCursor(25, 215);
+		gfx_puts(print_PILA);
+		gfx_setCursor(180, 215);
+		gfx_puts("V");
+		gfx_setTextColor(LCD_YELLOW, LCD_BLACK);
+		gfx_setCursor(5, 255);
+		gfx_puts("Serial/USB:");
+       		gfx_setTextColor(LCD_GREEN, LCD_BLACK);
+		gfx_setCursor(180, 255);
+		gfx_puts(SerialUSB);   
+
+		/* llamado para mostrar los cambios en la pantalla fisica */
 		lcd_show_frame();
 	}
 }
